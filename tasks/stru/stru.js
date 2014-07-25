69b7d3cc60c00f9e7ce21c514e50f35b6698e936
Greg Smith <gsmith@incompl.com>
2014-07-25 16:55:59 -0400

    nth commit

--- tasks/stru/stru.js
+++ tasks/stru/stru.js
@@ -62,100 +62,111 @@ module.exports = function(grunt) {
 
     // Interpret our STRU files
     fs.readdirSync(options.src).forEach(function(file) {
+      var fileContent = fs.readFileSync(path.join(options.src, file),
+                                       {encoding: 'utf8'});
+
       var html = templates['header.combyne'].render({
         title: 'Cool Site'
       });
-      var indentSize = 2;
-      var lastIndent = 0;
-      var lineNum = 0;
-      var stack = [];
-      var htmlQueue = null;
-      var stru = fs.readFileSync(path.join(options.src, file),
-                                 {encoding: 'utf8'});
-      var lines = stru.split(os.EOL);
-      lines.forEach(function(line) {
-        lineNum++;
-        if (line.match(/^\w*$/)) {
-          return;
-        }
-        var lineInfo = line.match(/^(\s*)([-|@#!])(.*)$/);
-        if (lineInfo === null) {
-          error('Syntax error', lineNum, line);
-        }
-        var lineIndent = lineInfo[1];
-        var lineCommand = lineInfo[2];
-        var lineOptions = lineInfo[3];
 
-        var thisIndent = lineIndent.length;
-        var indentDiff = lastIndent - thisIndent;
-        if (indentDiff % indentSize !== 0) {
-          error('Wrong indent level', lineNum, line);
-        }
-        var popCount = indentDiff / indentSize;
-        while (popCount > 0) {
-          html = pop(stack, html);
-          popCount--;
-        }
-        lastIndent = thisIndent;
-
-        if (htmlQueue !== null) {
-          if (lineCommand === '#') {
-            htmlQueue = htmlQueue.replace(/style="([^"]*)"/,
-                function(match, p1) {
-              return 'style="' + p1 +
-                     paramStyle(lineOptions, lineNum, line) + '"';
-            });
-          }
-          else if (lineCommand === '!') {
-            var unique = 'bp' + Math.round(Math.random() * 1000000);
-            htmlQueue = htmlQueue.replace(/<div/,
-                function(match, p1) {
-              return '<style>@media(max-width:' + lineOptions + ')' +
-                     '{.' + unique + ' {display: block;}}</style><div';
-            });
-            htmlQueue = htmlQueue.replace(/class="([^"]*)"/,
-                function(match, p1) {
-              return 'class="' + p1 + ' ' + unique + '"';
-            });
-          }
-          else {
-            html += htmlQueue;
-            htmlQueue = null;
-          }
-        }
-
-        if (lineCommand === '-') {
-          htmlQueue = '<div style="" class="row' + lineOptions + '">';
-          stack.push('row');
-        }
-        else if (lineCommand === '|') {
-          htmlQueue = '<div style="" class="col' + lineOptions + '">';
-          stack.push('col');
-        }
-        else if (lineCommand === '@') {
-          if (_(stack).last() === 'row') {
-            error('Include cannot be child of Row', lineNum, line);
-          }
-          html += include(lineOptions);
-        }
-
-      });
-
-      while (stack.length > 0) {
-        html = pop(stack, html);
-      }
-
-      var destFile = file.replace('.stru', '.html');
+      html += stru(fileContent, options);
 
       html += templates['footer.combyne'].render();
 
+      var destFile = file.replace('.stru', '.html');
       fs.writeFileSync(path.join(options.dest, destFile), html);
-
-      grunt.log.ok(file, '->', destFile);
-
+      grunt.log.ok(file, ' -> ', destFile);
     });
 
   });
+
+  function stru(string, options) {
+    var html = '';
+    var indentSize = 2;
+    var lastIndent = 0;
+    var lineNum = 0;
+    var stack = [];
+    var htmlQueue = null;
+
+    var lines = string.split(os.EOL);
+    lines.forEach(function(line) {
+      lineNum++;
+      if (line.match(/^\s*$/)) {
+        return;
+      }
+      var lineInfo = line.match(/^(\s*)((.)(.*))$/);
+      if (lineInfo === null) {
+        error('Syntax error', lineNum, line);
+      }
+      var lineIndent = lineInfo[1];
+      var lineContent = lineInfo[2];
+      var lineCommand = lineInfo[3];
+      var lineOptions = lineInfo[4];
+      var isInclude = lineContent.match(/^&\s*include/) !== null;
+      var isCollapse = lineContent.match(/^&\s*collapse/) !== null;
+
+      var thisIndent = lineIndent.length;
+      var indentDiff = lastIndent - thisIndent;
+      if (indentDiff % indentSize !== 0) {
+        error('Wrong indent level', lineNum, line);
+      }
+      var popCount = indentDiff / indentSize;
+      while (popCount > 0) {
+        html = pop(stack, html);
+        popCount--;
+      }
+      lastIndent = thisIndent;
+
+      if (htmlQueue !== null) {
+        if (lineCommand === '&' && !isInclude && !isCollapse) {
+          htmlQueue = htmlQueue.replace(/style="([^"]*)"/,
+              function(match, p1) {
+            return 'style="' + p1 +
+                   paramStyle(lineOptions, lineNum, line) + '"';
+          });
+        }
+        else if (lineCommand === '&' && isCollapse) {
+          var unique = 'bp' + Math.round(Math.random() * 1000000);
+          htmlQueue = htmlQueue.replace(/<div/,
+              function(match, p1) {
+            var maxWidth = lineOptions.replace(/collapse\s*/, '');
+            return '<style>@media(max-width:' + maxWidth + ')' +
+                   '{.' + unique + ' {display: block;}}</style><div';
+          });
+          htmlQueue = htmlQueue.replace(/class="([^"]*)"/,
+              function(match, p1) {
+            return 'class="' + p1 + ' ' + unique + '"';
+          });
+        }
+        else {
+          html += htmlQueue;
+          htmlQueue = null;
+        }
+      }
+
+      if (lineCommand === '@') {
+        htmlQueue = '<div style="" class="stru' + lineOptions + '">';
+        stack.push('row');
+      }
+      else if (isInclude) {
+        if (htmlQueue !== null) {
+          htmlQueue = htmlQueue.replace(/class="stru/, 'class="');
+        }
+        html += include(lineContent, lineNum, line);
+      }
+      else if (lineCommand.match(/^[@&]/) === null) {
+        html += marked(lineContent);
+      }
+
+    });
+
+    while (stack.length > 0) {
+      html = pop(stack, html);
+    }
+
+    return html;
+
+  }
 
   function pop(stack, html) {
     var type = stack.pop();
@@ -173,16 +184,37 @@ module.exports = function(grunt) {
 
   var included = {};
 
-  function include(file) {
-    file = file.trim();
+  function include(command, lineNum, line) {
+    var html;
+    var string;
+    var commandInfo = command.match(/\s*include\s*(\w+\.(\w+))/);
+    if (commandInfo === null) {
+      error('Invalid include syntax', lineNum, line);
+    }
+    var file = commandInfo[1];
+    var extension = commandInfo[2];
+
     if (included[file] !== undefined) {
       return included[file];
     }
-    var markdown = fs.readFileSync(path.join('content', file),
-                               {encoding: 'utf8'});
-    var html = marked(markdown);
-    included[file] = html;
-    return html;
+
+    if (extension === 'md') {
+      string = fs.readFileSync(path.join('content', file),
+                                     {encoding: 'utf8'});
+      html = '<div>' + marked(string) + '</div>';
+      included[file] = html;
+      return html;
+    }
+    else if (extension === 'stru') {
+      string = fs.readFileSync(path.join('content', file),
+                                   {encoding: 'utf8'});
+      html = '<div>' + stru(string) + '</div>';
+      included[file] = html;
+      return html;
+    }
+    else {
+      error('Unknown file type', lineNum, line);
+    }
   }
 
   function paramStyle(options, lineNum, line) {
